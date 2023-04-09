937b3afb044bf68c1dcb6ebe44531d66488a38db
yuzeng <yuzeng@umich.edu>
2023-04-09 12:44:15 -0400

    remove loss_masks since it is used for instance segmentation

--- models/invar_detr.py
+++ models/invar_detr.py
@@ -155,34 +155,34 @@ class SetCriterion(nn.Module):
         return losses
 
     
-    def loss_masks(self, outputs, targets, indices, num_boxes):
-        """Compute the losses related to the masks: the focal loss and the dice loss.
-           targets dicts must contain the key "masks" containing a tensor of dim [nb_target_boxes, h, w]
-        """
-        assert "pred_masks" in outputs
+    #def loss_masks(self, outputs, targets, indices, num_boxes):
+    #    """Compute the losses related to the masks: the focal loss and the dice loss.
+    #       targets dicts must contain the key "masks" containing a tensor of dim [nb_target_boxes, h, w]
+    #    """
+    #    assert "pred_masks" in outputs
 
-        src_idx = self._get_src_permutation_idx(indices)
-        tgt_idx = self._get_tgt_permutation_idx(indices)
-        src_masks = outputs["pred_masks"]
-        src_masks = src_masks[src_idx]
-        masks = [t["masks"] for t in targets]
-        # TODO use valid to mask invalid areas due to padding in loss
-        target_masks, valid = nested_tensor_from_tensor_list(masks).decompose()
-        target_masks = target_masks.to(src_masks)
-        target_masks = target_masks[tgt_idx]
+    #    src_idx = self._get_src_permutation_idx(indices)
+    #    tgt_idx = self._get_tgt_permutation_idx(indices)
+    #    src_masks = outputs["pred_masks"]
+    #    src_masks = src_masks[src_idx]
+    #    masks = [t["masks"] for t in targets]
+    #    # TODO use valid to mask invalid areas due to padding in loss
+    #    target_masks, valid = nested_tensor_from_tensor_list(masks).decompose()
+    #    target_masks = target_masks.to(src_masks)
+    #    target_masks = target_masks[tgt_idx]
 
-        # upsample predictions to the target size
-        src_masks = interpolate(src_masks[:, None], size=target_masks.shape[-2:],
-                                mode="bilinear", align_corners=False)
-        src_masks = src_masks[:, 0].flatten(1)
+    #    # upsample predictions to the target size
+    #    src_masks = interpolate(src_masks[:, None], size=target_masks.shape[-2:],
+    #                            mode="bilinear", align_corners=False)
+    #    src_masks = src_masks[:, 0].flatten(1)
 
-        target_masks = target_masks.flatten(1)
-        target_masks = target_masks.view(src_masks.shape)
-        losses = {
-            "loss_mask": sigmoid_focal_loss(src_masks, target_masks, num_boxes),
-            "loss_dice": dice_loss(src_masks, target_masks, num_boxes),
-        }
-        return losses
+    #    target_masks = target_masks.flatten(1)
+    #    target_masks = target_masks.view(src_masks.shape)
+    #    losses = {
+    #        "loss_mask": sigmoid_focal_loss(src_masks, target_masks, num_boxes),
+    #        "loss_dice": dice_loss(src_masks, target_masks, num_boxes),
+    #    }
+    #    return losses
         
 
     def _get_src_permutation_idx(self, indices):
@@ -201,8 +201,8 @@ class SetCriterion(nn.Module):
         loss_map = {
             'eq': self.loss_labels,
             'cardinality': self.loss_cardinality,
-            'op': self.loss_boxes,
-            'masks': self.loss_masks
+            'op': self.loss_boxes
+            #'masks': self.loss_masks
         }
         assert loss in loss_map, f'do you really want to compute {loss} loss?'
         return loss_map[loss](outputs, targets, indices, num_boxes, **kwargs)
