804f26c1c10e530360114cd0f2bbdd952b76f89f
yuzeng <yuzeng@umich.edu>
2023-06-10 11:38:15 -0400

    correct the DNN_CROSS_ENTROPY

--- simple_models/criterion.py
+++ simple_models/criterion.py
@@ -2,7 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class DNN_CROSS_ENTROPY(nn.module):
+class DNN_CROSS_ENTROPY(nn.Module):
     def forward(self, outputs, targets):
-        degree_labels = targets["max_degree"]
-        return F.cross_entropy(outputs, degree_labels)
+        # flatten the first dimension of outputs
+        outputs = outputs.view(-1, outputs.shape[-1])
+        degrees = []
+        for target in targets:
+            degrees.append(target['max_degree'])
+        return F.cross_entropy(outputs, degrees)
