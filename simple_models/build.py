27fddba8889a83682f132b1d622160fd1277aa50
yuzeng <yuzeng@umich.edu>
2023-06-14 00:14:14 -0400

    add transformer model

--- simple_models/build.py
+++ simple_models/build.py
@@ -1,11 +1,17 @@
 from .dnn import DNN
 from .accuracy import count_accuracy
 from .criterion import DNN_CROSS_ENTROPY
+from .transformer_encoder import MyTransformer
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 def build_dnn_model(args):
     model = DNN(args.max_var_num)
+    criterion = DNN_CROSS_ENTROPY()
+    return model, criterion, count_accuracy
+
+def build_transformer_model(args):
+    model = MyTransformer()
     criterion = DNN_CROSS_ENTROPY()
     return model, criterion, count_accuracy
--- simple_models/transformer_encoder.py
+++ simple_models/transformer_encoder.py
@@ -1,50 +1,26 @@
-# the model here is generated from GPT. It should be revised
-
 import torch
-import torch.nn as nn
-import math
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
-
+from torch import nn
 
 class MyTransformer(nn.Module):
-    def __init__(self, input_dim, num_classes, nhead, nhid, nlayers, dropout):
+    def __init__(self, d_model=512, nhead=8, num_layers=2, num_classes=3):
         super(MyTransformer, self).__init__()
-
-        self.embedding = nn.Linear(input_dim, input_dim)
-        self.pos_encoder = PositionalEncoding(input_dim, dropout)
+        self.d_model = d_model
+        self.nhead = nhead
+        self.num_layers = num_layers
+        self.num_classes = num_classes
 
         self.transformer_encoder = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(d_model=input_dim, nhead=nhead, dim_feedforward=nhid, dropout=dropout),
-            num_layers=nlayers
+            nn.TransformerEncoderLayer(d_model, nhead),
+            num_layers
         )
 
-        self.fc = nn.Linear(input_dim, num_classes)
+        self.classifier = nn.Linear(d_model, num_classes)
 
-    def forward(self, x):
-        x = x.view(x.size(0), -1, x.size(1))
-        x = self.embedding(x)
-        x = self.pos_encoder(x)
-        x = x.transpose(0, 1)
-        x = self.transformer_encoder(x)
-        x = self.fc(x[-1, :, :])
+    def forward(self, src, masks):
+        output = self.transformer_encoder(src)
+        output = self.classifier(output)
+        return output
 
-        return x
 
 # Initialize the model
-model = MyTransformer(input_dim=512, num_classes=3, nhead=4, nhid=2048, nlayers=2, dropout=0.2)
+model = MyTransformer()
